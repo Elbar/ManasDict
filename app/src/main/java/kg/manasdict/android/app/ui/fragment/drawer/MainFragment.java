fec638536e1f55202a392fe2595cb085257c0153
Vista <vista-e@mail.ru>
2016-04-02 16:30:39 +0600

    *Added TextWatcher -> MainFragment| Modified Main Fragment

--- app/src/main/java/kg/manasdict/android/app/ui/fragment/drawer/MainFragment.java
+++ app/src/main/java/kg/manasdict/android/app/ui/fragment/drawer/MainFragment.java
@@ -1,32 +1,44 @@
 package kg.manasdict.android.app.ui.fragment.drawer;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.AppCompatSpinner;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.mikepenz.iconics.view.IconicsCompatButton;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import kg.manasdict.android.R;
 
 /**
  * Created by root on 3/31/16.
  */
-public class MainFragment extends Fragment implements View.OnClickListener {
+public class MainFragment extends Fragment implements View.OnClickListener, TextWatcher{
 
     private AppCompatSpinner mSourceLangSpinner;
     private AppCompatSpinner mDestinationLangSpinner;
     private IconicsCompatButton mExchangeLangBtn;
     private int mLastSourceLangItemId = 0;
     private int mLastDestinationLangItemId = 1;
-
+    private EditText mSearchWord;
+    private Timer mTimer;
+    private final long TIMER_DELAY = 1000;
     @Nullable
     @Override
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         initFragmentElements(rootView);
@@ -43,15 +55,55 @@ public class MainFragment extends Fragment implements View.OnClickListener {
         }
     }
 
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        if (mTimer != null) {
+            mTimer.cancel();
+        }
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+
+        if (s.length() != 0) {
+//            Log.d(MainFragment.class.getName(), s.toString());
+            mTimer = new Timer();
+            mTimer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getActivity(), "lorem", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            }, TIMER_DELAY);
+
+        }
+
+
+    }
+
     protected void initFragmentElements(View rootView) {
         mSourceLangSpinner = (AppCompatSpinner) rootView.findViewById(R.id.sourceLangSpinner);
         mDestinationLangSpinner = (AppCompatSpinner) rootView.findViewById(R.id.destinationLangSpinner);
         mExchangeLangBtn = (IconicsCompatButton) rootView.findViewById(R.id.exchangeLangBtn);
+        mSearchWord = (EditText) rootView.findViewById(R.id.searchText);
+        mTimer = new Timer();
 
         ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(
-                                                            getActivity(),
-                                                            R.array.spinner_languages,
-                                                            android.R.layout.simple_spinner_dropdown_item);
+                getActivity(),
+                R.array.spinner_languages,
+                android.R.layout.simple_spinner_dropdown_item);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mSourceLangSpinner.setAdapter(spinnerAdapter);
         mDestinationLangSpinner.setAdapter(spinnerAdapter);
@@ -88,6 +140,7 @@ public class MainFragment extends Fragment implements View.OnClickListener {
             }
         });
         mExchangeLangBtn.setOnClickListener(this);
+        mSearchWord.addTextChangedListener(this);
 
         mSourceLangSpinner.setSelection(0);
         mDestinationLangSpinner.setSelection(1);
